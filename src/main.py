a159940ad7bd089f0d32ac9ac007ab9b9103ba47
Gossepojke <svedlund.oskar@gmail.com>
2022-09-21 15:28:11 +0200

    income and expediture added as attributes

--- src/main.py
+++ src/main.py
@@ -5,10 +5,28 @@
 
 from resources import Establishment, Shop, Resturant
 
-test1 = Shop("test_1", "undecided", ["marget", "elias", "markus"], ["idk", "apples"])
+test1 = Shop("test_1", "undecided", ["marget", "elias", "markus"], 100, 50,["idk", "apples"])
 
 def main():
-    test1.change_name()
+
+    while True:
+        choice = input("1. check name\n2. check agreement\n3. check employies\n4. Hire additional employees\n#. exit\n>> ")
+
+        if choice == "1":
+            print(test1.name)
+
+        elif choice == "2":
+            print(test1.agrement)
+
+        elif choice == "3":
+            print(test1.employees)
+
+        elif choice == "4":
+            test1.hire_staff()
+
+        elif choice == "#":
+            break
+
 
 if __name__ == "__main__":
     main()
--- src/resources.py
+++ src/resources.py
@@ -5,41 +5,42 @@
 
 
 class Establishment:
-    def __init__(self, name, agreement, employees : list) -> None:
+    def __init__(self, name, agreement, employees : list, income : int, expenditure : int) -> None:
         self.name = name
         self.agrement = agreement
         self.employees = employees
+        self.income = income
+        self.expenditure = expenditure
 
     def change_name(self):
         self.name = input(">> ")
 
     def hire_staff(self):
-        pass
+        self.employees.append(input("Name the new employee\n>> "))
 
     def change_agrement(self):
-        pass
+        self.agrement = input("New agreement\n>> ")
 
+    def change_income(self, new_income):
+        self.income = new_income
+
+    def change_expenditure(self, new_expenditure):
+        self.expenditure = new_expenditure
 
 
 class Resturant(Establishment):
-    def __init__(self, name, agrement, employees: list, menu : list) -> None:
-        super().__init__(name, agrement, employees)
+    def __init__(self, name, agrement, employees: list, income : int, expenditure : int, menu : list) -> None:
+        super().__init__(name, agrement, employees, income, expenditure)
         self.menu = menu
 
     def check_menu(self):
         return self.menu
 
 
-
-    
 class Shop(Establishment):
-
-    def __init__(self, name, agrement, employees, inventory : list) -> None:
-        super().__init__(name, agrement, employees)
+    def __init__(self, name, agrement, employees, income : int, expenditure : int,inventory : list) -> None:
+        super().__init__(name, agrement, employees, income, expenditure)
         self.inventory = inventory
 
     def check_inventory(self):
         return self.inventory
-
-
-    
