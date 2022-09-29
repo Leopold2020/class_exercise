a9bee6ab0467ab41e9c6e63829120eb819661963
Gossepojke <svedlund.oskar@gmail.com>
2022-09-29 09:51:10 +0200

    done with the classes, now stating with the main

--- src/class_exrecise/heros-and-villans.py
+++ src/class_exrecise/heros-and-villans.py
@@ -42,8 +42,17 @@ class Villan:
     def get_identity(self):
         self.seceret_identity = input("")
 
+
     def reveal_plan(self):
         return self.seceret_plan
+
+    def recruit_hencmen(self, new_hench: list):
+        self.new_hench = new_hench
+        # name = input("Henchmen name\n>> ")
+        # salary = int(input("Salary\n>> "))
+        # role = input("Role\n>> ")
+
+        self.henchmen.append(Henchmen(self.new_hench))
     
     
 
@@ -54,3 +63,52 @@ class Henchmen:
         self.works_for = works_for
         self.salary = salary
         self.role = role
+
+
+#----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+def make_hero():
+    heroname = input("Hero name\n>> ")
+    heroidentity = input("Seceret identity\n>> ")
+    heroweakness = input("Weakness\n>> ")
+    herochatchphrase = input("Catch phrase\n>> ")
+
+
+    return Hero(heroname, heroidentity, heroweakness, herochatchphrase, False, None)
+
+def make_villan():
+    villanname = input("Villan name\n>> ")
+    villanidentity = input("Seceret identity\n>> ")
+    villanhideout = input("Seceret hideout\n>> ")
+    villanplan = input("")
+
+
+    return Villan(villanname,)
+
+
+def main():
+    choice1 = input("What do you whant to do?\n>> ")
+
+    if choice1 == "1":
+
+        newperson = input("New person\n1. Hero\n2. Villan>>")
+
+        if newperson == "1":
+            make_hero()
+
+
+        elif newperson == "2":
+            villanname = input("Villan name\n>> ")
+
+    
+
+
+if __name__ == "__main__":
+    main()
